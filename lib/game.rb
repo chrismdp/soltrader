2317cde98fa690f7a4b208516d72bd548a6f8b8a
Chris Parsons <chris@thinkcodelearn.com>
2011-10-29 21:25:43 +0100

    Use tested code

--- lib/game.rb
+++ lib/game.rb
@@ -2,7 +2,18 @@ class Game < Chingu::Window
   def initialize
     super(800, 600, false)
     Gosu::Image.autoload_dirs << File.join(File.dirname(__FILE__), "..", "media")
-    puts Gosu::Image.autoload_dirs.inspect
     @player = Player.create(:x => 200, :y => 200, :image => Image[ "spaceship.png" ])
+    @player.input = {
+      :holding_up => :go_faster,
+      :holding_down => :go_slower,
+      :holding_left => :turn_left,
+      :holding_right => :turn_right
+    }
+    self.input = { :escape => :exit }
+  end
+
+  def update
+    super
+    self.caption = "FPS: #{self.fps} milliseconds_since_last_tick: #{self.milliseconds_since_last_tick}"
   end
 end
--- spacestuff.rb
+++ spacestuff.rb
@@ -2,27 +2,8 @@ require "rubygems"
 require "bundler/setup"
 
 require 'chingu'
-
 include Gosu
 
-class Game < Chingu::Window
-  def initialize
-    super(800, 600, false)
-    self.input = { :escape => :exit }
-    @factor = 2
-    @player = Player.create(:x => 200, :y => 200, :image => Image[ "spaceship.png" ])
-    @player.input = {
-      :holding_up => :go_faster,
-      :holding_down => :go_slower,
-      :holding_left => :turn_left,
-      :holding_right => :turn_right
-    }
-  end
-
-  def update
-    super
-    self.caption = "FPS: #{self.fps} milliseconds_since_last_tick: #{self.milliseconds_since_last_tick}"
-  end
-end
+require_relative "lib/spacestuff"
 
 Game.new.show
