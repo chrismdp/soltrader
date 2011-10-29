daa9aaf1d1d91149d54d940219724ffa25b91c19
Chris Parsons <chris@thinkcodelearn.com>
2011-10-29 20:08:06 +0100

    Basic asteroids style movement

---
+++ README.md
@@ -0,0 +1,4 @@
+Space Stuff
+-----------
+
+A cool game made by Alyssa, Christopher and Chris Parsons.
---
+++ spacestuff.rb
@@ -0,0 +1,57 @@
+require "rubygems"
+require "bundler/setup"
+
+require 'chingu'
+
+include Gosu
+
+class Game < Chingu::Window
+  def initialize
+    super(800, 600, false)
+    self.input = { :escape => :exit }
+    @player = Player.create(:x => 200, :y => 200, :image => Image[ "spaceship.png" ])
+    @player.input = {
+      :holding_up => :go_faster,
+      :holding_down => :go_slower,
+      :holding_left => :turn_left,
+      :holding_right => :turn_right
+    }
+  end
+end
+
+class Player < Chingu::GameObject
+  trait :velocity
+
+  def initialize(options)
+    @rate_of_acceleration = 0.4
+    super(options)
+  end
+
+  def turn_left
+    @angle -= 10
+  end
+
+  def turn_right
+    @angle += 10
+  end
+
+  def go_faster
+    angle_rad = @angle * Math::PI / 180.0
+    @velocity_y += -Math.cos(angle_rad) * @rate_of_acceleration
+    @velocity_x += Math.sin(angle_rad) * @rate_of_acceleration
+  end
+
+  def go_slower
+    angle_rad = @angle * Math::PI / 180.0
+    @velocity_y -= -Math.cos(angle_rad) * @rate_of_acceleration
+    @velocity_x -= Math.sin(angle_rad) * @rate_of_acceleration
+  end
+
+  def update
+    super
+    @velocity_y *= 0.95
+    @velocity_x *= 0.95
+  end
+end
+
+Game.new.show
