4a76d1fdb549d3cb4ed0ef435757b1cfcf1a6ca6
Chris Parsons <chris@thinkcodelearn.com>
2011-11-14 09:46:20 +0000

    Refactored to use throttling code

--- lib/game/ship_ai.rb
+++ lib/game/ship_ai.rb
@@ -3,24 +3,16 @@ module Spacestuff
     class ShipAi
       def initialize(options)
         @ship = options[:ship]
-        @next_run = 0
         @target = nil
       end
 
       def update(seconds_elapsed)
-        elapsed = seconds_elapsed * 1000.0
-        @next_run -= elapsed
-        if (@next_run <= 0)
-          @next_run = rand(200) + 900
-          @target = find_target
-        end
-
-        if (@target && @next_run < 500)
-          track
-        end
+        throttle(:find, rand(200) + 900, seconds_elapsed * 1000.0) { find_target }
+        throttle(:track, 20, seconds_elapsed * 1000.0) { track }
       end
 
       def track
+        return unless @target
         angle_to_other = Gosu::angle(@ship.x, @ship.y, @target.x, @target.y)
         diff = Gosu::angle_diff(@ship.angle * 180.0 / Math::PI + 90, angle_to_other)
         if (diff < -5)
