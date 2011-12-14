7ccdb495a874d0f78cdaa104371c65085320cc0d
Chris Parsons <chris@thinkcodelearn.com>
2011-12-14 12:54:39 +0000

    Made locations a hash not an array

--- lib/gamestates/space.rb
+++ lib/gamestates/space.rb
@@ -2,31 +2,48 @@ module Sol
   module Gamestates
     class Space < Chingu::GameState
       trait :viewport
+
+      def setup_locations
+        #f = File.open("data/sol.yml")
+        #data = YAML.load(f)
+        #data["locations"].each do |record|
+          #@locations << Sol::Game::Location.new(location.symbolize_keys)
+        #end
+        #data["jump_gates"].each do |record|
+          #@jump_gates << Sol::Game::JumpGate.new(record.symbolize_keys)
+        #end
+
+        @locations = {}
+        @locations[:earth_orbit] = Sol::Game::Location.new(:name => "Earth Orbit", :width => 10000, :height => 10000)
+        @locations[:mars_orbit] = Sol::Game::Location.new(:name => "Mars Orbit", :width => 10000, :height => 10000)
+
+        jump_gates = {}
+        jump_gates[:earth_mars] = Sol::Game::JumpGate.new(:position => vec2(5000, 4500), :location => @locations[:earth_orbit])
+        jump_gates[:mars_earth] = Sol::Game::JumpGate.new(:position => vec2(5000, 4500), :location => @locations[:mars_orbit])
+
+        jump_gates[:earth_mars].connect_to(jump_gates[:mars_earth])
+
+        Sol::Game::CelestialBody.new(:position => vec2(5000,5000), :location => @locations[:earth_orbit], :image => 'earth.png', :name => 'Earth')
+        Sol::Game::CelestialBody.new(:position => vec2(5000,4000), :location => @locations[:mars_orbit], :image => 'mars.png', :name => 'Mars')
+      end
+
       def initialize
         super
-        @locations = []
-        @locations << Sol::Game::Location.new(:name => "Earth Orbit", :width => 10000, :height => 10000)
-
-        @locations << Sol::Game::Location.new(:name => "Mars Orbit", :width => 10000, :height => 10000)
-        earth_gate = Sol::Game::JumpGate.new(:position => vec2(5000, 4500), :location => @locations.first)
-        mars_gate = Sol::Game::JumpGate.new(:position => vec2(5000, 4500), :location => @locations[1])
-        earth_gate.connect_to(mars_gate)
-
-        earth = Sol::Game::CelestialBody.new(:position => vec2(5000,5000), :location => @locations.first, :image => 'earth.png', :name => 'Earth')
-        mars = Sol::Game::CelestialBody.new(:position => vec2(5000,4000), :location => @locations[1], :image => 'mars.png', :name => 'Mars')
+        setup_locations
 
         @schematic = Sol::Game::Schematic.new
         @schematic.draw(Sol::Game::HullPiece.new(:x => 0, :y => 0, :width => 48, :height => 48))
         @schematic.draw(Sol::Game::CockpitPiece.new(:x => 3, :y => 2, :width => 5, :height => 5))
         @schematic.draw(Sol::Game::EnginePiece.new(:x => 2, :y => 4, :width => 5, :height => 5))
         @schematic.draw(Sol::Game::EnginePiece.new(:x => 4, :y => 4, :width => 5, :height => 5))
-        @player_ship = Sol::Game::Ship.new(:schematic => @schematic, :x => 5000, :y => 5050, :location => @locations.first)
+
+        @player_ship = Sol::Game::Ship.new(:schematic => @schematic, :x => 5000, :y => 5050, :location => @locations[:earth_orbit])
 
         @minds = []
         #add_ships(100)
 
         self.viewport.lag = 0.95
-        self.viewport.game_area = [0, 0, @locations.first.width, @locations.first.height]
+        self.viewport.game_area = [0, 0, @locations[:earth_orbit].width, @locations[:earth_orbit].height]
         self.input = {
           :e => :enter_planet,
           :holding_up => :go_faster,
@@ -41,10 +58,10 @@ module Sol
 
       def add_ships(x)
         x.times do
-          ship = Sol::Game::Ship.new(:schematic => @schematic, :x => rand(3000) + 3500, :y => rand(3000) + 3500, :location => @locations.first)
+          ship = Sol::Game::Ship.new(:schematic => @schematic, :x => rand(3000) + 3500, :y => rand(3000) + 3500, :location => @locations[:earth_orbit])
           actor = Sol::Ai::Actor.new :behaviours => [:travel]
           actor.take_controls_of(ship)
-          actor.destination = @locations[1]
+          actor.destination = @locations[:mars_orbit]
           @minds << actor
         end
       end
@@ -91,7 +108,7 @@ module Sol
         super
         elapsed = $window.milliseconds_since_last_tick
         @minds.each { |ai| ai.update(elapsed) }
-        @locations.each { |l| l.update(elapsed) }
+        @locations.values.each { |l| l.update(elapsed) }
 
         @stars.update(viewport)
         self.viewport.center_around(@player_ship)
@@ -107,9 +124,9 @@ module Sol
       def draw
         super
         @stars.draw
+        @font ||= Font["good-times.ttf", 35]
 
         if (@player_ship.in_gate?)
-          @font ||= Font["good-times.ttf", 35]
           @font.draw("Hyperspace", 200, 200, 2)
           @font.draw("Jumping to #{@player_ship.destination}", 200, 250, 2)
           @font.draw("ETA %.1f" % [@player_ship.time_to_destination_in_seconds], 200, 300, 2)
@@ -117,7 +134,6 @@ module Sol
         end
 
         if (@player_ship.landed?)
-          @font ||= Font["good-times.ttf", 35]
           @font.draw("LANDED ON #{@player_ship.planet.name}", 200, 200, 2)
           return
         end
